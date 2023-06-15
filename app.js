8b9c08cdb7aef5330da8be400c2bc84f4fa186d0
oneness <bera.anamika961@gmail.com>
2023-06-15 16:39:50 +0530

    changes

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.use((err, req, res, next) => {
   })
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 10000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
