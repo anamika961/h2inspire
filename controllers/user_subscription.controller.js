b142fd79399c5a3abc5037ead6f52de8c0056c9e
anamika961 <bera.anamika961@gmail.com>
2023-09-20 18:15:03 +0530

    pdf invoice and mail

--- controllers/user_subscription.controller.js
+++ controllers/user_subscription.controller.js
@@ -5,6 +5,8 @@ const PackageType = require("../models/package_type.model");
 const path = require("path");
 const fs = require("fs");
 const { generateInvoicePdf } = require('../utils/pdf-generator');
+const moment = require('moment-timezone');
+const nodemailer = require("nodemailer");
 
 module.exports = {
     listbyId: async (req, res, next) => {
@@ -78,13 +80,125 @@ module.exports = {
              const subscription_data = new UserSubscription(req.body)
              const result = await subscription_data.save();
 
-             const fileName = Date.now()+ '.pdf'
-          //  const filePath = path.join(__dirname, `../../uploads/invoices/${fileName}`);
+             let subscriptionData = await UserSubscription.findOne({_id:result?._id}).populate([
+                 {
+                     path:"package",
+                     select:"",
+                     populate:{
+                        path:"package_type",
+                        select:"name"
+                     }
+                 },
+                 {
+                    path:"employer",
+                    select:""
+                }
+             ]);
+
+             let empEmail = subscriptionData?.employer?.email;
+
+             let empPhoneNo = subscriptionData?.employer?.mobile;
+
+
+             let packageList = await Package.findOne({_id:subscriptionData?.package});
+
+             let packageTypeData = await PackageType.findOne({_id:subscriptionData?.package?.package_type});
+
+             let packaeName = packageTypeData?.name
+
+             console.log({packaeName});
+            
+             let amount;
+             if(packaeName == "PAY AS YOU GO"){
+                amount = (packageList?.payAsYou_detail?.amount) * subscriptionData?.quantity
+             }else if(packaeName == "BUSINESS"){
+                amount = (packageList?.business_detail?.amount)
+             }else if(packaeName == "SCALE"){
+                amount = (packageList?.scale_detail?.amount)
+             }
+             console.log({amount})
+
+
+
+            const invoiceNo =  "INV" + Math.floor(Math.random() * 90000) + 10000;
+            const subDate = moment(subscriptionData?.createdAt).format("DD/MM/YYYY");
+            const totalAmount = subscriptionData?.total_amount
+            
+
+            const fileName = Date.now()+ '.pdf'
             const filePath = path.join(__dirname, `../uploads/invoices/${fileName}`);
+
+            console.log({filePath});
         
-            // orderPrice = Number(orderPrice).toFixed(2)
-            const invoiceDetails = { amount:"20" };
+            const invoiceDetails = { invoiceNo, subDate, packaeName, totalAmount, amount, empEmail, empPhoneNo};
             generateInvoicePdf(invoiceDetails, filePath);
+
+            var transport = nodemailer.createTransport({
+                host: "mail.demo91.co.in",
+                port: 465,
+                auth: {
+                  user: "developer@demo91.co.in",
+                  pass: "Developer@2023"
+                }
+            });
+            var mailOptions = {
+                from: 'developer@demo91.co.in',
+                to: `bera.amamika961@gmail.con`,
+                subject: `Package purchase successfully`,
+                html: `
+                <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Purchase of "Pay As You Go" Package</title>
+                </head>
+                    <body>
+                        <p><strong>Subject:</strong> Purchase of "Pay As You Go" Package for Hiri2Inspire - INR 499</p>
+
+                        <p>Dear [Purchasing Team's Name],</p>
+
+                        <p>I hope this message finds you well. I am writing on behalf of Hiri2Inspire to request the purchase of the "Pay As You Go" package, priced at INR 499. We believe that this package will greatly benefit our organization and help us achieve our goals more efficiently.</p>
+
+                        <p>The "Pay As You Go" package offers several key advantages for our team, including:</p>
+
+                        <ol>
+                            <li><strong>Cost-Effective:</strong> With this package, we only pay for the services we use, which aligns well with our budgetary considerations.</li>
+                            <li><strong>Flexibility:</strong> It provides us with the flexibility to scale our usage up or down based on our evolving needs, ensuring that we always have access to the resources required for our projects.</li>
+                            <li><strong>Easy Management:</strong> The package comes with a user-friendly management interface that will allow us to monitor our usage, track expenses, and make adjustments as needed.</li>
+                            <li><strong>Support:</strong> We also appreciate the support and assistance that comes with this package, ensuring that we have access to help whenever necessary.</li>
+                        </ol>
+
+                        <p>We have thoroughly reviewed the package details and believe that it is the ideal solution for our organization's current requirements. To proceed with the purchase, we kindly request that you initiate the process as soon as possible.</p>
+
+                        <p>Please let us know if there are any specific procedures or paperwork that we need to complete on our end to facilitate this purchase. If you require any additional information or have any questions, please feel free to reach out to us at [Your Contact Information].</p>
+
+                        <p>We are excited about the possibilities that the "Pay As You Go" package offers, and we look forward to working with your team to finalize this purchase.</p>
+
+                        <p>Thank you for your prompt attention to this matter.</p>
+
+                        <p>Warm regards,</p>
+
+                        <p>[Your Name]<br>
+                        [Your Title]<br>
+                        Hiri2Inspire<br>
+                        [Your Contact Information]</p>
+                    </body>
+                `,
+                attachments: [
+                    { 
+                        filename: fileName,
+                        content: fs.createReadStream(filePath)
+                    }
+                ]
+            };
+              
+            transport.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log('Email sent: ' + info.response);
+                }
+            });
             return res.status(200).send({
                 error: false,
                 message: "User subscribed successfully",
--- utils/pdf-generator.js
+++ utils/pdf-generator.js
@@ -1,29 +1,209 @@
 const fs = require("fs");
 const PDFDocument = require("pdfkit");
 
+    // generateHeader(doc);
+    // generateCustomerInformation(doc, invoice);
+    // generateInvoiceTable(doc, invoice);
+    // generateFooter(doc);
+
 function generateInvoicePdf(invoice, path) {
+   
     let doc = new PDFDocument({ size: "A4", margin: 50 });
 
-    generateHeader(doc);
-   // generateCustomerInformation(doc, invoice);
-    generateInvoiceTable(doc, invoice);
-    // generateFooter(doc);
+
+    doc.fontSize(16).text('Invoice', { align: 'center' });
+
+    doc.moveDown(1); // Move down one line before adding the subject
+
+    const subjectText = 'Payment for ' + invoice.packaeName;
+    const textWidth = doc.widthOfString(subjectText);
+    const textX = (doc.page.width - textWidth) / 2; // Center the text horizontally
+    const textY = doc.y; // Get the current Y position
+    doc.text(subjectText, textX, textY);
+
+    const underlineY = textY + doc.currentLineHeight() + 5; // Adjust the value to control the underline position
+    doc.lineWidth(1).moveTo(textX, underlineY).lineTo(textX + textWidth, underlineY).stroke();
+
+    doc.fontSize(12).text('Date: ' + invoice.subDate, 50, doc.y + 20);
+    doc.fontSize(12).text('Invoice No: ' + invoice.invoiceNo , 380, doc.y);
+
+
+    // Add the billing address
+    const billingAddress = `Your Billing Address Here\nEmail: ${invoice.empEmail}\nPhone No: ${invoice.empPhoneNo}`;
+    doc.fontSize(12).text('Billing Address:', 50, doc.y + 20);
+    doc.font('Helvetica').fontSize(10).text(billingAddress, 50, doc.y + 5, {
+        width: doc.page.width - 100, // Limit the width to keep text within the page
+        align: 'left',
+    });
+
+    const tableHeader = ['Description', 'Amount'];
+    const tableX = 50;
+    const tableY = doc.y + 20; // Position below the billing address
+    const columnWidth = (doc.page.width - 100) / tableHeader.length; // Divide available width evenly
+    
+    
+
+    doc.fontSize(12);
+    doc.text(tableHeader[0], tableX, tableY);
+    const amountColumnX = doc.page.width - columnWidth - 50; // Place the "Amount" column on the right side
+    doc.text(tableHeader[1], amountColumnX + 180, tableY);
+
+    doc.lineWidth(1).moveTo(tableX, tableY + 15).lineTo(tableX + doc.page.width - 100, tableY + 15).stroke();
+
+    // Add table data (one row)
+    const tableData = [invoice.packaeName, invoice.amount];
+    doc.text(tableData[0], tableX, doc.y + 20);
+    doc.text(tableData[1], tableX + columnWidth + 180, doc.y - 15);
+
+    const gstAmount = "18%"; // Replace with your GST amount
+    const totalAmount = invoice.totalAmount; // Replace with your total amount
+    doc.text(`GST: ${gstAmount}`, amountColumnX + 150, doc.y + 20);
+    doc.text(`Total: ${totalAmount}`, amountColumnX + 150, doc.y + 5);
+
+    const footerText = 'Your Footer Text Here';
+    doc.fontSize(10).text(footerText, doc.page.margins.left + 200, doc.page.height - 100);
+
+
+
+
+    // Add the invoice number on the right side with a font size of 12px
+    // const invoiceNumber = 'Your Invoice Number Here';
+    // const invoiceNumberWidth = doc.widthOfString(invoiceNumber);
+    // const invoiceNumberX = doc.page.width - invoiceNumberWidth - 50;
+    // doc.text('Invoice Number: ' + invoiceNumber, invoiceNumberX, doc.y);
+
+    
+    
 
     doc.end();
     doc.pipe(fs.createWriteStream(path));
 }
 
 function generateHeader(doc) {
-    doc
-        //.image("uploads/logo.png", 50, 45, { width: 50 })
-        .fillColor("#444444")
-        .fontSize(20)
-        .text("Hire2Inspire", 110, 57)
-        .fontSize(10)
-        .text("Kolkata 700001", 200, 65, { align: "right" })
-        .text("India", 200, 80, { align: "right" })
-        .moveDown();
+//     doc
+//         //.image("uploads/logo.png", 50, 45, { width: 50 })
+//         .fillColor("#444444")
+//         .fontSize(20)
+//         .text("Hire2Inspire", 110, 57)
+//         .fontSize(10)
+//         .text("Kolkata 700001", 200, 65, { align: "right" })
+//         .text("India", 200, 80, { align: "right" })
+//         .moveDown();
+// }
+
+
+// const PDFDocument = require('pdfkit');
+// const fs = require('fs');
+
+// Create a new PDF document
+// const doc = new PDFDocument();
+let invoiceData ={};
+const ChnageFormatDate = (d) =>{
+    return d;
 }
+const DueDate = (d) =>{
+   return d ;
+}
+let EmpData = {} ;
+const GSTIN = (d) =>{
+    return d;
+}
+// Pipe the PDF content to a writable stream (e.g., a file)
+const outputStream = fs.createWriteStream('invoice.pdf');
+doc.pipe(outputStream);
+
+// Set up your fonts and other styling here
+doc.font('Helvetica');
+doc.fontSize(11);
+
+// Add Logo
+// doc.image('uploads/logo.png', 50, 45, { width: 50 });
+
+// Title
+doc.fillColor("#444444").fontSize(20).text('TAX INVOICE', 110, 57);
+
+// Company Address
+doc.fontSize(10).text('Kolkata 700001', 200, 65, { align: 'right' });
+doc.text('India', 200, 80, { align: 'right' });
+
+// Billing Address
+const billingAddress = [
+    'Hire2Inspire',
+    'Bangalore',
+    'Phone: 9876456789',
+    'Email: info@hire2inspire.com'
+];
+doc.text(billingAddress.join('\n'), 50, 120);
+
+// Invoice Number and Dates
+doc.text(`Invoice Number: ${invoiceData?.invoice_No}`, 400, 120, { align: 'right' });
+doc.text(`Invoice Date: ${ChnageFormatDate(new Date(invoiceData?.billing_id?.createdAt))}`, 400, 135, { align: 'right' });
+doc.text(`Due Date: ${DueDate(invoiceData?.billing_id?.createdAt)}`, 400, 150, { align: 'right' });
+
+//Client Address
+const clientAddress = [
+    'Invoice Issued For-',
+    `Name: ${EmpData?.fname} ${EmpData?.lname}`,
+    `Company: ${invoiceData?.billing_id?.hire_id?.comp_name.toUpperCase()}`,
+    `Address: ${invoiceData?.billing_id?.address?.substring(0, 30)}`,
+    `Phone: ${EmpData?.mobile}`,
+    `Email: ${invoiceData?.billing_id?.email}`,
+    `${EmpData?.company_website_url}`
+];
+doc.text(clientAddress.join('\n'), 50, 190);
+
+GSTIN
+const gstinText = `GSTIN: ${invoiceData?.gst_in}`;
+doc.fontSize(8).text(gstinText, 50, 320);
+
+// Subject Line
+const subjectLine = 'Subject: Invoice from Hire2Inspire for Hiring Candidate.';
+doc.fontSize(10).text(subjectLine, 50, 350);
+
+//Candidate Billing Address
+const candidateBillingAddress = [
+    'Candidate Information:',
+    // `Name: ${invoiceData?.candidate?.fname} ${invoiceData?.candidate?.lname}`,
+    // `Date of Joining: ${ChnageFormatDate(new Date(invoiceData?.billing_id?.hire_id?.date_of_joining))}`,
+    // `Designation offered: ${invoiceData?.billing_id?.hire_id?.desg_offered}`,
+    // `CTC offered : ${invoiceData?.billing_id?.hire_id?.comp_offered}`,
+    // `Company Name: ${invoiceData?.billing_id?.hire_id?.comp_name}`
+];
+doc.fontSize(9);
+for (let i = 0; i < candidateBillingAddress.length; i++) {
+    const line = candidateBillingAddress[i];
+    doc.text(line, 50, 380 + i * 20);
+}
+
+
+// Finalize the PDF document
+
+
+// // Calculate total GST and other amounts
+// const totalAmount = invoiceData?.split_amount?.h2i_amount + invoiceData?.split_amount?.agency_amount;
+// const gstPercentage = 18;
+// const gstAmount = totalAmount * (gstPercentage / 100);
+
+// // Display total amount and GST
+// doc.fillColor('#0C356A').fontSize(10).text(`Total Amount: ${totalAmount.toFixed(2)} rupees`, 400, tableY + 20, { align: 'right' });
+// doc.text(`GST (${gstPercentage}%): ${gstAmount.toFixed(2)} rupees`, 400, tableY + 40, { align: 'right' });
+
+// // Display final amount
+// doc.font('Helvetica-Bold').text(`Final Amount: ${invoiceData?.amount.toFixed(2)} rupees`, 400, tableY + 60, { align: 'right' });
+
+// // Add Logo at the bottom right corner
+// doc.image('uploads/h2iSign.png', 350, 700, { width: 100 });
+
+// // Add company name and authorized signature
+// doc.fontSize(10).text('For Sant Sales India Pvt Ltd', 50, 700);
+// doc.fontSize(10).text('Authorized Signature', 50, 720);
+
+// // Finalize the PDF document
+// doc.end();
+
+// console.log('PDF generated successfully.');
+}
+
 
 // function generateCustomerInformation(doc, invoice) {
 //     doc
