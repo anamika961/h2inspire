cd79aeea41fe83f33fbd58cafe7116ee4ef7436b
anamika961 <bera.anamika961@gmail.com>
2024-01-11 15:03:16 +0530

    work on package

--- controllers/employer.controller.js
+++ controllers/employer.controller.js
@@ -313,10 +313,10 @@ module.exports = {
         },
         {
             path:"package",
-            select:"",
+            select:" ",
             populate:{
               path:"package_type",
-              select:"name"
+              select:" "
             }
         }
     ]).sort({_id:-1});
